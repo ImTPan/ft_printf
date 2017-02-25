64789587363f1ca123fe587f15a6752f2c6b346f
Thomas Pan <tpan@e1z3r6p23.42.us.org>
2017-02-25 12:49:04 -0800

    tired

---
+++ ft_printf/write_chars.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   write_chars.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tpan <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/02/25 12:20:05 by tpan              #+#    #+#             */
+/*   Updated: 2017/02/25 12:48:52 by tpan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	write_chars(t_conversion *conversion, va_list ap,
+						t_format *format)
+{
+	char	*draft;
+
+	if (conversion->flags.hash)
+		return ;
+	draft = ft_strnew(1);
+}
