5b7e80ae5678feafaf355b8a64723277715609df
Thomas Pan <tpan@e1z3r6p23.42.us.org>
2017-02-26 19:34:05 -0800

    testing apply width

--- ft_printf/apply_width.c
+++ ft_printf/apply_width.c
@@ -6,7 +6,7 @@
 /*   By: tpan <tpan@student.42.us.org>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/02/26 16:07:51 by tpan              #+#    #+#             */
-/*   Updated: 2017/02/26 17:19:51 by tpan             ###   ########.fr       */
+/*   Updated: 2017/02/26 19:19:09 by tpan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,13 @@
 
 static void		apply_width_str(t_conversion *conversion, char **draft)
 {
-	
+	char *temp;
+
+	if(conversion->width > ft_strlen(*draft))
+	{
+
+	}
+	return ;
 }
 
 static void		apply_width_int(t_conversion *conversion, char **draft)
